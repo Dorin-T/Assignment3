684bcca259fa02a2c31513e2652dbe340cee7405
Louise Mikkelsen <lollem@hotmail.com>
2017-10-10 15:22:37 +0200

    Revert "dont use this"
    This reverts commit a4c47e86fa3d414ab426389ae16e1942bc574977.

--- Program.cs
+++ Program.cs
@@ -1,25 +1,16 @@
 ﻿using System;
-using System.Diagnostics.PerformanceData;
 
 namespace Assignment3
 {
-    public class Category
-    {
-        private int cid { get; set; }
-        private string name { get; set; }
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            List<Category> categories = new List<Category>();
-            categories.Add(new Category() {cid = 1, name = "Beverages"});
-            categories.Add(new Category() {cid = 2, name = "Condiments"});
-            categories.Add(new Category() {cid = 3, name = "Confections"});
-            
-            Console.WriteLine("Hello World! " + categorries[1]);
+            List<string> category = new List<string>();
+            category.Add("Beverages");
+            category.Add("Condiments");
+            category.Add("Confections");
+            Console.WriteLine("Hello World! " + category);
         }
     }
-
-   
 }
