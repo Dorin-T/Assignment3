a4c47e86fa3d414ab426389ae16e1942bc574977
Louise Mikkelsen <lollem@hotmail.com>
2017-10-10 15:21:05 +0200

    dont use this

--- Program.cs
+++ Program.cs
@@ -1,16 +1,25 @@
 ﻿using System;
+using System.Diagnostics.PerformanceData;
 
 namespace Assignment3
 {
+    public class Category
+    {
+        private int cid { get; set; }
+        private string name { get; set; }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            List<string> category = new List<string>();
-            category.Add("Beverages");
-            category.Add("Condiments");
-            category.Add("Confections");
-            Console.WriteLine("Hello World! " + category);
+            List<Category> categories = new List<Category>();
+            categories.Add(new Category() {cid = 1, name = "Beverages"});
+            categories.Add(new Category() {cid = 2, name = "Condiments"});
+            categories.Add(new Category() {cid = 3, name = "Confections"});
+            
+            Console.WriteLine("Hello World! " + categorries[1]);
         }
     }
+
+   
 }
